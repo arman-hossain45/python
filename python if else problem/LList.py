62998b7b50d68b905daa360faf4bd0a69a33c8d8
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-09-02 23:52:58 +0600

    list  comprehension

--- python if else problem/LList.py
+++ python if else problem/LList.py
@@ -1,7 +1,32 @@
-#list are mutable 
+# # #list are mutable 
 
-s1=[34,56,"arman",True] #here t is must capital alphabet
-print(s1[0])
-print(s1)
+# # s1=[34,56,"arman",True] #here t is must capital alphabet
+# # # print(s1[0])
+# # # print(s1)
 
-print(s1[-2])
+# # # print(s1[-2])
+
+
+# # # if 89 in s1 :
+# # #     print("yes")
+# # # else :
+# # #     print("no")
+
+# # marks=[3,4,5,"arman","eleveen","f1"]
+# # print(marks)
+# # print(marks[:])
+# # print(marks[1:])
+
+# # print (marks[1:-1])
+# # print(marks[0:3:])
+
+# #list comprehension
+# # lst=[i for i in range (4)]
+# # print(lst)
+
+# lst2=[i*i for i in range(10)if i%2==0]
+# print(lst2)
+
+names=["milo","sarah","arman","meem","alex","h"]
+names_with=[item for item in names if len(item)<2]
+print(names_with)
