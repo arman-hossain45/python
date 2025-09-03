a5d1f22d1412207f8d287fbeccc7a721f24394d7
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-09-03 18:57:49 +0600

    f-string

---
+++ python if else problem/doc-string.py
@@ -0,0 +1,9 @@
+#docstring is a method to see comments we use docstring in a function below where we declaire a function 
+
+def square (n):
+    "takes in a number n, and return the square of n"
+    print(n**2)
+
+square(5)
+print(square.__doc__)#we can show the text in this output
+
---
+++ python if else problem/f-string.py
@@ -0,0 +1,12 @@
+# in f string method we use for formating code in specific terms 
+
+letter="hey my name is {} and I am from {}"
+country="bangladesh"
+name="arman"
+print(letter.format(name,country))
+
+#we declaire in this way for better understanding 
+
+print(f"hey my name is {name} and I am form {country}")
+
+print(f"hey my name is {name} and i am form {country}")
