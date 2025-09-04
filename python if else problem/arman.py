a3e54a15d5f6c4e0e20f075b3adbc26fde2cdfcd
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-09-04 12:11:10 +0600

    Add welcome function implementation in arman.py and ensure single execution on import

---
+++ python if else problem/arman.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("hey you are welcome")
+
+# This part runs only if you execute arman.py directly,
+# not when importing it from another file.
+if __name__ == "__main__":
+    welcome()
+# for this method when we run this file by import other file then it prints one time function call dont run two times of function call
---
+++ python if else problem/mainArman.py
@@ -0,0 +1,5 @@
+import arman
+arman.welcome()#we create a file named arman.py and import it here to use its function
+#arman.welcome()#we can call the function like this also but here is a problem when we import arman file it prints two times 
+# of hey you are welcome but we want to print one time this message
+
