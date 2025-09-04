798ab9ae4d562830076a4f27420c1b667f9d5833
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-09-04 11:51:54 +0600

    Add initial implementation of math operations and module exploration in importWorks.py

---
+++ python if else problem/importWorks.py
@@ -0,0 +1,16 @@
+import math
+
+res = math.sqrt(16)
+print(res)
+
+
+from math import sqrt, pi
+res = sqrt(25)*pi
+print(res)
+
+from math import pi,sqrt as sq#if we want to use a method or part in my code in several time then use as keyword
+res = sq(36)*pi
+print(res)
+
+
+print(dir(math))# it show us all the methods and attributes in math module
