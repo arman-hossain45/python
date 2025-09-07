9b58302b8740d3da227b9333b1e40d0c794eb456
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-09-07 11:40:10 +0600

    Add initial implementation of person class with constructor and info method

---
+++ python if else problem/oop.py/class/constructor.py
@@ -0,0 +1,15 @@
+class person:
+    def __init__(self):
+        print("this is constructor")
+
+
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
+
+a=person()#create object when we create object it automatically call constructor
+
+
+
+
+        
+    
