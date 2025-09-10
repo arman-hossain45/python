d9ecdf0902d95c56cbae50123a431389f64de8af
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-09-10 11:33:12 +0600

    Refactor ExceptionHandling.py to include try-except blocks for error handling and improve input validation; update arman.ipynb with structured code and execution metadata.

--- python if else problem/ExceptionHandeling.py
+++ python if else problem/ExceptionHandeling.py
@@ -5,9 +5,11 @@ for i in range(1,11):
 
 print("some implementation lines of code")
 print("end of thr program ")
+
 #here in this code when we input a string or another digit then it show us a error 
 #and another code is not exucute but we want to exucute another code and show us this error message 
 #so for this reason we use try and except block
+
 try:
     a=int(input("Enter a number: "))
     print(f"The number you entered is {a}is")
