826b9710ca0ad548e4497f24477df8dde77e87e7
Hysenburg <freedomfighterit588@gmail.com>
2025-09-11 17:24:46 +0600

    Remove @classmethod decorator from changeCompany method in porter class

--- python if else problem/oop.py/class/classMethod.py
+++ python if else problem/oop.py/class/classMethod.py
@@ -4,7 +4,7 @@ class porter:
     def show(self,name):
         self.name=name
         print(F"The employee  name is : {self.name}\nCompany name is {self.company}")
-    @classmethod
+    
     def changeCompany(cls,newcompany):
         cls.company=newcompany
 
@@ -14,3 +14,6 @@ a.changeCompany("tesla")
 a.show("arman")
 b=porter()
 b.show("shawon")
+
+#in class method use to we modify the class variable and other method 
+#when we use @classmethod it modify the the varible for each  object
