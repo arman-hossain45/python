7377f8eda227d410968d50fc184f2499122ac484
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-09-11 10:11:57 +0600

    Add access modifier examples and student class implementation; demonstrate private and protected access in Python.

--- python if else problem/oop.py/class/classObj.py
+++ python if else problem/oop.py/class/classObj.py
@@ -195,5 +195,48 @@
 
 
 
+# #access modifier  private  accerss modifier 
+
+# class employee:
+#     def __init__ (self,):
+#         self.__name="arman"
+
+
+# a=employee()
+# print(a.__name)#cannot be accessable
+
+# print(a._employee__name)#acceable through mangling in private data 
+
+# class Employee:
+#     def __init__(self):
+#         self.__name = "arman"
+
+# a = Employee()
+# print(a._Employee__name)  ->arman
+
+#protected
+
+# class student:
+#     def __init__ (self):
+#         self.name="arman"
+
+#     def _fullname(self):
+#         #protected access modifier 
+#         return "hello world"
+
+# class subject(student):
+#     pass
+
+# obj=student()
+
+# obj2=subject()
+
+# print(obj._fullname())
+
+
+# print(obj2._fullname())#protected data access by child class 
+
+
+
 
 
