be758b2881059bffcd52e14de3ffe15a8a9dfaac
Hysenburg <freedomfighterit588@gmail.com>
2025-09-13 18:30:28 +0600

    Add initial implementation of dellMethod.py with seek functionality documentation

---
+++ python if else problem/seek/dellMethod.py
@@ -0,0 +1,12 @@
+#seek is work to read values in a file f.seek(10)  means in 10 values then read f.read(5) read 5 value start with 10 
+
+
+# with open ('file.txt','r') as f:
+#   print(type(f))
+
+# f.seek(10)
+# read the next 5 bytes
+#data=f.read(5)
+#print(data)#read 5 value which is start at 10 no value 
+# whwn program is too big then if we want to know that which part of this program seek()is call 
+#then we call the method of seek()
